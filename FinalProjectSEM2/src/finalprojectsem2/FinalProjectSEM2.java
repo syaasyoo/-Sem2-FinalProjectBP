a3fe9a9f048a1694b508b40a606a60ea7112ed09
Aisyah Azzahra <21082010024@student.upnjatim.ac.id>
2022-06-22 09:41:02 +0700

    Create FinalProjectSEM2.java

---
+++ FinalProjectSEM2/src/finalprojectsem2/FinalProjectSEM2.java
@@ -0,0 +1,20 @@
+/*
+ * 
+ * yh kalo kata aku mah semangadh ajh..
+ */
+package finalprojectsem2;
+
+/**
+ *
+ * @author Aisyah Azzahra
+ */
+public class FinalProjectSEM2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
