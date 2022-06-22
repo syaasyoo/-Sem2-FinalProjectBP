e077fa41b91e1c9d120298bcd61d7bedbb99136f
Aisyah Azzahra <21082010024@student.upnjatim.ac.id>
2022-06-22 10:06:03 +0700

    Update FinalProjectSEM2.java

--- FinalProjectSEM2/src/finalprojectsem2/FinalProjectSEM2.java
+++ FinalProjectSEM2/src/finalprojectsem2/FinalProjectSEM2.java
@@ -3,18 +3,18 @@
  * yh kalo kata aku mah semangadh ajh..
  */
 package finalprojectsem2;
-
+import java.util.Scanner;
 /**
  *
  * @author Aisyah Azzahra
  */
 public class FinalProjectSEM2 {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("TESSSS");
     }
     
 }
