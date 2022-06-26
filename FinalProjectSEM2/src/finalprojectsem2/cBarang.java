bdc5999b0b7f599604ab19c49b723cce517ccdca
Aisyah Azzahra <21082010024@student.upnjatim.ac.id>
2022-06-26 13:00:44 +0700

    Create cBarang.java

---
+++ FinalProjectSEM2/src/finalprojectsem2/cBarang.java
@@ -0,0 +1,18 @@
+
+package finalprojectsem2;
+
+public class cBarang {
+    String nama;
+    int harga;
+
+    public cBarang(String n, int h) {
+        nama = n;
+        harga = h;
+    }
+    public String getNama(){
+        return nama;
+    }
+    public int getHarga(){
+        return harga;
+    }
+}
