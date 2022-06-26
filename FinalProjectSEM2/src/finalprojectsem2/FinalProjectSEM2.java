096fe2ea1299a19119c6c9d6d9230f91cfa74968
alyafatinf <96181287+alyafatinf@users.noreply.github.com>
2022-06-26 11:22:11 +0700

    Delete FinalProjectSEM2.java

--- FinalProjectSEM2/src/finalprojectsem2/FinalProjectSEM2.java
+++
@@ -1,9 +0,0 @@
-package finalprojectsem2;
-import java.util.Scanner;
-public class FinalProjectSEM2 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("TESSSS");
-    }
-   // so smngat yh, nti tak kasi kadepku klo fpnya selese 
-}
