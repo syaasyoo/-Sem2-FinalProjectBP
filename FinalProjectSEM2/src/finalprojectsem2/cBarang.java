07dc2cf27c44819974c07c4a34f6dec88f2e35b7
Aisyah Azzahra <21082010024@student.upnjatim.ac.id>
2022-06-26 12:32:07 +0700

    Update cBarang.java

--- FinalProjectSEM2/src/finalprojectsem2/cBarang.java
+++ FinalProjectSEM2/src/finalprojectsem2/cBarang.java
@@ -1,13 +1,20 @@
-/*
- * 
- * yh kalo kata aku mah semangadh ajh..
- */
+
 package finalprojectsem2;
 
-/**
- *
- * @author Aisyah Azzahra
- */
 public class cBarang {
+    String nama;
+    int harga;
+
+    public cBarang(String n, int h) {
+        nama=n;
+        harga=h;
+    }
+    public String getNama(){
+        return nama;
+    }
+    public  int getHarga(){
+        return harga;
+    }
+
     
 }
